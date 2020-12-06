8101e9a009d7ad9fb712d0cb3a0b5fefa48264ae
Riley van <rileyvan@gmail.com>
2020-12-06 16:21:20 +0000

    q2 a)

--- exercise_2/q2/svmlin.py
+++ exercise_2/q2/svmlin.py
@@ -21,6 +21,54 @@ def svmlin(X, t, C):
     # result   : result of classification     (1 x num_samples)
     # slack    : points inside the margin (boolean)   (1 x num_samples)
 
+    # Number of data points
+    epsilon = 1e-6
+    n, d = X.shape[0], X.shape[1]
+    H = np.zeros((n, n))
 
-    #####Insert your code here for subtask 2a#####
+    # H(n, m) = t_n * t_m * x_m^T x x_n
+    for i in range(0, n):
+        for j in range(0, n):
+            H[i][j] = t[i] * t[j] * np.inner(X[j, :], X[i, :])
+    
+    H = cvxopt.matrix(H, (n, n))
+    # Multiplier for a_n
+    q = cvxopt.matrix(-1*np.ones(n), (n, 1))
+
+    # Bound a's between a lower and upper Bound
+    LB, UB = np.zeros(n), C*np.ones(n)
+    G = cvxopt.matrix(np.vstack([-np.eye(n), np.eye(n)]), (2*n, n))
+    h = cvxopt.matrix(np.hstack([-LB, UB]), (2*n, 1))
+
+    # constraint
+    A = cvxopt.matrix(t, (1, n))
+    b = cvxopt.matrix(0.0)
+
+    # solve it
+    alpha = np.array(cvxopt.solvers.qp(H, q, G, h, A, b)['x'])[:,0]
+
+    # boolean array indicating if point is a support vector
+    sv = ((alpha >= epsilon) & (alpha <= C))
+
+    # W is linear combination of a_n * t_n * x_n
+    w = np.zeros(d)
+    for i in range(0, n):
+        w += alpha[i] * t[i] * X[i,:]
+
+    # calculate offset, average over all suport vectors
+    b = 0
+    for i in range(0, n):
+        if sv[i]:
+            b += t[i]
+            for j in range(0, n):
+                if sv[j]:
+                    b -= alpha[j] * t[j] * np.inner(X[j], X[i])
+    b /= np.sum(sv)
+
+    # classify the points 
+    result = X @ w.T + b*np.ones(n)
+
+    # points that are in the margin. 
+    slack = sv & (np.absolute(t - result) > epsilon)
+   
     return alpha, sv, w, b, result, slack
